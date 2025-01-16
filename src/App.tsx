66e49dae0c5a380ca3a53d4c98346ea75b544ef0
Spacs21 <behaksr123@Gmail.com>
2025-01-16 17:25:11 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='text-5xl text-blue-600 font-bold text-center'>Final Exams!</h2>
+    </>
+  )
+}
+
+export default App
